5716fa7f4e7ac5f3ccd00ec4590c097856f4b27a
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 08:31:35 -0800

    A

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right - inserting node to the right
+ *@parent: Our root
+ *@value: the node value
+ *Return: 
+**/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		fprintf(stderr, "FAILURE");
+		return (NULL);
+	}
+
+	new_node= binary_tree_insert_right(parent, value);
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "FAILURE");
+		return (NULL);
+	}
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent-right;
+		parent->right->node = new_node;
+	}
+	parent->new_node;
+	return (new_node);
+}
