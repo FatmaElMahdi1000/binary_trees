8a9e457dc5e88b918447a72cd962b2a99e3eed60
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 04:05:19 -0800

    A

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,5 +20,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		result->parent = parent;
 		result->left = NULL;
 		result->right = NULL;
-		return (result)
+		return (result);
 	}
+}
