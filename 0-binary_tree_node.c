8e50de85cec1e00a2f4423a55fdec63dbd795229
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 03:31:36 -0800

    A

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,9 +5,21 @@
  *@value: value of the node
  * Return: the new node
  **/
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *result = malloc(sizeof(binary_tree_t));
 
-
+	if (result == NULL)
+	{
+		fprintf(stderr, "FAILURE, Could not allocate a memory for the node");
+		return (NULL);
+	}
+	else
+	{
+		result->n = value;
+		result->parent = NULL;
+		result->left = NULL;
+		result->right = NULL;
+		return (result);
+	}
 }
