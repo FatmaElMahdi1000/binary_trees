33de536c0501d2acdc9303e68b46d65a37335bd9
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 02:44:07 -0800

    A

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,13 @@
+#include "binary.h"
+/**
+ *binary_tree_node - creation binary node
+ *@parent: the root node
+ *@value: value of the node
+ * Return: the new node
+ **/
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+
+}
