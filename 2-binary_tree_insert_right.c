b0823e017b103da0662dd3e18c9c25e36170c839
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 08:46:56 -0800

    A

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,11 +12,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		fprintf(stderr, "FAILURE");
 		return (NULL);
 	}
 
-	new_node = binary_tree_insert_right(parent, value);
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 	{
