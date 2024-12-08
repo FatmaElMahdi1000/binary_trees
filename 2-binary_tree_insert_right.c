62cc3fd69ea55869caba5886405e51791d07c46c
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-08 08:39:10 -0800

    A

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -29,6 +29,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = parent->right;
 		parent->right->node = new_node;
 	}
-	parent->new_node;
+	parent->right = new_node;
 	return (new_node);
 }
